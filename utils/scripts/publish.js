368cc6a833870c9ede1480d3389dd53fd1167e0d
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2021-03-02 12:16:19 -0600

    chore(scripts): update publishing script to tag through lerna (#19)

--- utils/scripts/publish.js
+++ utils/scripts/publish.js
@@ -42,31 +42,64 @@ const publishToNpm = async () => {
   spinner.start().succeed('Published to NPM');
 };
 
+// const OLD_tagRelease = async () => {
+//   spinner.start('Generating new release tag');
+
+//   const date = new Date();
+//   const day = ('0' + date.getDate()).slice(-2);
+//   const month = ('0' + (date.getMonth() + 1)).slice(-2);
+//   const year = date.getFullYear();
+//   const releaseDate = `${year}-${month}-${day}`;
+
+//   const tagArgs = [
+//     'tag',
+//     '-a',
+//     `r${releaseDate}`,
+//     '-m',
+//     `${releaseDate} release`,
+//   ];
+//   await execa('git', tagArgs);
+
+//   spinner.succeed();
+//   spinner.start('Pushing generated tag');
+
+//   const pushArgs = ['push', '--follow-tags', 'origin'];
+//   await execa('git', pushArgs);
+
+//   spinner.succeed('Pushed tag');
+// };
+
+/**
+ * Tell lerna to tag the new release but undo the package version
+ * commit becasue it was already done by the release script
+ */
 const tagRelease = async () => {
-  spinner.start('Generating new release tag');
+  spinner.start('Generating new release tags');
 
-  const date = new Date();
-  const day = ('0' + date.getDate()).slice(-2);
-  const month = ('0' + (date.getMonth() + 1)).slice(-2);
-  const year = date.getFullYear();
-  const releaseDate = `${year}-${month}-${day}`;
-
-  const tagArgs = [
-    'tag',
-    '-a',
-    `r${releaseDate}`,
-    '-m',
-    `${releaseDate} release`,
+  const lernaArgs = [
+    'lerna',
+    'version',
+    '--conventional-commits', // version packages by conventional commits
+    '--no-changelog', // don`t generate changelog
+    '--no-push', // don`t push changes (commit changes and add tags)
+    '--yes', // answer yes to all
   ];
-  await execa('git', tagArgs);
 
-  spinner.succeed();
-  spinner.start('Pushing generated tag');
+  spinner.info();
+  await execa('yarn', lernaArgs, {
+    stdin: process.stdin,
+    stdout: process.stdout,
+  });
+
+  spinner.start('Pushing generated tags');
+
+  // Remove lerna package changes. We only want the tags
+  await execa('git', ['reset', '--hard', 'HEAD~1']);
 
   const pushArgs = ['push', '--follow-tags', 'origin'];
   await execa('git', pushArgs);
 
-  spinner.succeed('Pushed tag');
+  spinner.succeed('Pushed generated tags');
 };
 
 const run = async () => {
